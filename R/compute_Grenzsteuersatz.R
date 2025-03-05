3b23bcf2ae183ebed9bb383e1b59a737bfc312b0
lmuehlen <lmuehlen@uni-muenster.de>
2025-03-05 17:46:19 +0100

    0.01

--- R/compute_Grenzsteuersatz.R
+++ R/compute_Grenzsteuersatz.R
@@ -1,5 +1,5 @@
 
-#' Compute Grenzsteuersatz
+#'
 #'
 #' *compute_Grenzsteuersatz* berechnet den Grenzsteuersatz bei einem zu versteuernden Einkommen von *zve* basierend auf den gegebenen Eckwerten der Grenzsteuer.
 #'
@@ -10,15 +10,14 @@
 #' @param pg1e Ende erste Progressionszone
 #' @param pg2s Startsatz zweite Progressionszone
 #' @param pg2e Ende zweite Progressionszone
-#' @param pp1s Satz erste Proportionalzone (Spitzensteuersatz)
+#' @param pp1s Satz erste Proportionalzone, Spitzensteuersatz
 #' @param pp1e Ende erste Proportionalzone
-#' @param pp2s Seite zweite Proportionalzone (Reichensteuersatz)
+#' @param pp2s Seite zweite Proportionalzone, Reichensteuersatz
 #'
 #' @returns number
 #' @export
 #'
-#' @examples
-#' compute_Grenzsteuersatz(30000)
+#' @examples compute_Grenzsteuersatz(30000)
 compute_Grenzsteuersatz <- function(zve, gfb=11604, pg1s=14, pg1e=17005, pg2s=23.97,
                             pg2e=66760, pp1s=42, pp1e=277826, pp2s=45) {
   mtr <- numeric(length(zve))
