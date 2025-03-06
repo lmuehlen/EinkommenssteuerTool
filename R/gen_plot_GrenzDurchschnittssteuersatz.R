dbf539b6f27f9da018e90d0743e93caeff179a03
lmuehlen <lmuehlen@uni-muenster.de>
2025-03-06 15:53:46 +0100

    0.01

---
+++ R/gen_plot_GrenzDurchschnittssteuersatz.R
@@ -0,0 +1,55 @@
+#' Plot Grenz- und Durchschnittssteuersatz
+#'
+#' @param df dataframe des Reformvorschlags
+#' @param max_x Beschränkung X-Achse (ZvE)
+#' @param max_y Beschränkung Y-Achse (Steuersatz)
+#' @param breaks
+#'
+#' @importFrom dplyr pull
+#' @import ggplot2
+#'
+#' @returns plot
+#' @export
+#'
+#' @examples
+#' \dontrun{gen_plot_GrenzDurchschnittssteuersatz(SPD2025_df)}
+gen_plot_GrenzDurchschnittssteuersatz<-function(df=NULL,max_x=125000,max_y=NULL,breaks=c(0,30000,60000,90000,120000)){
+
+  df<-df%>%filter(ZvE<=max_x)
+
+  if(fast){
+    df%>%filter()
+  }
+
+  labels=formatC(breaks,big.mark = ".",decimal.mark = ",", format = "f", digits = 0)
+  #Grenzen Plots
+
+  if(is.null(gstdst_ub)){
+    max_y<-df%>%pull(Grenzsteuersatz_inkl_Soli_vgl)%>%max(na.rm = T)/10%>%ceiling()*10
+    #gstdst_ub<-10*ceiling(max(df$Grenzsteuersatz_inklSoli_vgl,na.rm=T)/10)
+  }
+
+  vis_gst_dst<-df%>%
+    ggplot(aes(x=ZvE))+
+    geom_hline(yintercept = 0)+
+    #Grenzsteuersatz
+    geom_line(aes(y=Grenzsteuersatz_inklSoli_base,color="Status quo"))+
+    geom_line(aes(y=Grenzsteuersatz_inklSoli_vgl,color="Reform"))+
+    #Durchschnittssteuersatz
+    geom_line(aes(y=Durchschnittssteuersatz_inklSoli_base,color="Status quo"),linetype="dashed")+
+    geom_line(aes(y=Durchschnittssteuersatz_inklSoli_vgl,color="Reform"),linetype="dashed")+
+    #Design
+    scale_x_continuous(limits = c(0,max_y),expand = c(0.01,0.01),breaks=breaks,labels = labels)+
+    scale_y_continuous(limits = c(0,max_y))+
+    scale_color_manual(
+      values = c("Reform" = "#EE6174", "Status quo" = "#181c44")
+    )+
+    guides(color=guide_legend(position="inside"))+
+    labs(x=NULL,y=NULL,title="Grenz- und Durchschnittssteuersatz",subtitle = "in Prozent")+
+    theme_dz(base_size = 10)+
+    theme(
+      legend.position.inside = c(0.84, 0.19),  # Legende in die untere linke Ecke verschieben
+      legend.background = element_rect(fill = "transparent",color="transparent"),  # Hintergrund halbtransparent
+      legend.title = element_blank() )
+  return(vis_gst_dst)
+}
---
+++ R/theme_dz.R
@@ -0,0 +1,21 @@
+#' Ggplot theme Dezernat Zukunft
+#'
+#' @param base_size base size text
+#'
+#' @returns theme
+#' @export
+#'
+#' @examples
+#' \notrun{
+#' theme_dz()}
+theme_dz<-function(base_size=base_size){
+  theme_tufte(base_family = "Open Sans",base_size = base_size)+
+    theme(panel.grid.major.y = element_line(linetype = "11",linewidth=0.5,color="#181c44"),
+          axis.ticks.y = element_blank(),
+          axis.ticks.x =element_line(color="#181c44"),
+          legend.position = "bottom",
+          text=element_text( color="#181c44"),
+          axis.text=element_text( color="#181c44"),
+          plot.title = element_text(margin = margin(b = 2)),
+          plot.subtitle = element_text(margin = margin(t = 0)))
+}
