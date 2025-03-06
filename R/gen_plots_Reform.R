7ac1c5028e7bd7c6114d6d680b15d919bef32e00
lmuehlen <lmuehlen@uni-muenster.de>
2025-03-06 18:20:53 +0100

    0.01

--- R/gen_plots_Reform.R
+++ R/gen_plots_Reform.R
@@ -16,7 +16,7 @@
 #' \dontrun{
 #' get_plots_Reform(SPD2025_df)
 #' }
-gen_plots_Reform<-function(df,breaks_gstdst=c(0,30000,60000,90000,120000),breaks_ent=c(10000,30000,100000,500000),max_x_gstdst=125000,max_y_gstdst=60,max_x_ent=500000,max_y_ent=NULL,max_val=2,fast=FALSE){
+gen_plots_Reform<-function(df,breaks_gstdst=c(0,30000,60000,90000,120000),breaks_ent=c(10000,30000,100000,500000),max_x_gstdst=125000,max_y_gstdst=60,max_x_ent=500000,max_y_ent=NULL,width_ent=2,max_val=8,fast=FALSE){
   vis_gstdst<- gen_plot_GrenzDurchschnittssteuersatz(df,breaks=breaks_gstdst,max_x_gstdst,max_y_gstdst,fast=fast)
   vis_entpc<-gen_plot_relativeEntlastung(df,breaks=breaks_ent,max_x=max_x_ent,max_y=max_y_ent,fast=fast)
   vis_einkvert<-gen_plot_EntlastungEinkommensverteilung(df,breaks=breaks_ent,max_x=max_x_ent,width=width_ent,max_val=max_val)
