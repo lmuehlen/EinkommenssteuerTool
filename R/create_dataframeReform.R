f9f112b081be487756586be331d44512fe1661d3
lmuehlen <lmuehlen@uni-muenster.de>
2025-03-06 22:04:33 +0100

    0.01

--- R/create_dataframeReform.R
+++ R/create_dataframeReform.R
@@ -47,17 +47,18 @@ create_dataframeReform<-function(
   AnzahlSteuerpflichte_over1mio<-data_ZvE%>%filter(ZvE_lb==1000000)%>%pull(Steuerpflichtige_Gesamt)
   ZvEDurchschnitt_over1mio<-data_ZvE%>%filter(ZvE_lb==1000000)%>%pull(ZvE_Durchschnitt)
   #Durschnittliche zu zahlende Einkommensteuer für Steuerpflichtige mit zu versteuernden Einkommen über 1 Mio (Status quo und Reform)
-  Einkommensteuer_over1mio_base<-compute_Einkommensteuer(ZvEDurchschnitt_over1mio,gfb_base, pg1s_base, pg1e_base, pg2s_base, pg2e_base, pp1s_base, pp1e_base, pp2s_base)
-  Einkommensteuer_over1mio_reform<-compute_Einkommensteuer(ZvEDurchschnitt_over1mio,gfb_reform, pg1s_reform, pg1e_reform, pg2s_reform, pg2e_reform, pp1s_reform, pp1e_reform, pp2s_reform)
+ # Einkommensteuer_over1mio_base<-compute_Einkommensteuer(ZvEDurchschnitt_over1mio,gfb_base, pg1s_base, pg1e_base, pg2s_base, pg2e_base, pp1s_base, pp1e_base, pp2s_base)
+  #Einkommensteuer_over1mio_reform<-compute_Einkommensteuer(ZvEDurchschnitt_over1mio,gfb_reform, pg1s_reform, pg1e_reform, pg2s_reform, pg2e_reform, pp1s_reform, pp1e_reform, pp2s_reform)
   #Steuereinnahmen durch Steuerpflichtige mit zu versteuernden Einkommen über 1 Mio
-  Einnahmen_over1mio_base<-AnzahlSteuerpflichte_over1mio*Einkommensteuer_over1mio_base
-  Einnahmen_over1mio_reform<-AnzahlSteuerpflichte_over1mio*Einkommensteuer_over1mio_reform
-  Mindereinnahmen_over1mio<-Einnahmen_over1mio_base-Einnahmen_over1mio_reform
+  #Einnahmen_over1mio_base<-AnzahlSteuerpflichte_over1mio*Einkommensteuer_over1mio_base
+  #Einnahmen_over1mio_reform<-AnzahlSteuerpflichte_over1mio*Einkommensteuer_over1mio_reform
+  #Mindereinnahmen_over1mio<-Einnahmen_over1mio_base-Einnahmen_over1mio_reform
 
   #Creating dataset####
 
   df<-data_vertZvE%>%
-    mutate(ZvE=ZvE, #Anpassung!!!
+    add_row(ZvE=ZvEDurchschnitt_over1mio,Steuerpflichtige_norm_smooth=AnzahlSteuerpflichte_over1mio)%>%
+    mutate(ZvE=ZvE,
            Steuerpflichtige_norm_smooth,
            Grenzsteuersatz_base=compute_Grenzsteuersatz(ZvE,gfb_base, pg1s_base, pg1e_base, pg2s_base, pg2e_base, pp1s_base, pp1e_base, pp2s_base),
            Einkommensteuer_base=compute_Einkommensteuer(ZvE,gfb_base, pg1s_base, pg1e_base, pg2s_base, pg2e_base, pp1s_base, pp1e_base, pp2s_base),
@@ -71,7 +72,6 @@ create_dataframeReform<-function(
            Entlastung_absolut=Einkommensteuer_base-Einkommensteuer_reform,
            Mindereinnahmen=Einnahmen_base-Einnahmen_reform
     )%>%
-    add_row(Mindereinnahmen=Mindereinnahmen_over1mio)%>%
     mutate(
       #SoliGrenzsteuersatz
       Soli_Grenzsteuersatz_base=case_when(
