578e96e6080e57815e572feaeb4114782ba7f448
lmuehlen <lmuehlen@uni-muenster.de>
2025-03-06 12:48:11 +0100

    0.01

--- R/create_dataframeReform.R
+++ R/create_dataframeReform.R
@@ -1,6 +1,6 @@
 #' Datensatz für Reformvorschlag
 #'
-#' @param data_eink Tabelle 73111-02 aus Lohn- und Einkommenssteuerbericht
+#' @param data_eink Tabelle 73111-02 aus Lohn- und Einkommensteuerbericht
 #' @param gfb_reform Grundfreibetrag Reform
 #' @param pg1s_reform Startsatz erste Progressionszone
 #' @param pg1e_reform Ende erste Progressionszone
@@ -46,12 +46,12 @@ create_dataframeReform<-function(
   #Anzahl und durchschnittliche zu versteuerndes Einkommen Steuerpflichtige mit zu versteuernden Einkommen über 1 Mio
   AnzahlSteuerpflichte_over1mio<-data_ZvE%>%filter(ZvE_lb==1000000)%>%pull(Steuerpflichtige_Gesamt)
   ZvEDurchschnitt_over1mio<-data_ZvE%>%filter(ZvE_lb==1000000)%>%pull(ZvE_Durchschnitt)
-  #Durschnittliche zu zahlende Einkommenssteuer für Steuerpflichtige mit zu versteuernden Einkommen über 1 Mio (Status quo und Reform)
-  Einkommenssteuer_over1mio_base<-compute_Einkommensteuer(ZvEDurchschnitt_over1mio,gfb_base, pg1s_base, pg1e_base, pg2s_base, pg2e_base, pp1s_base, pp1e_base, pp2s_base)
-  Einkommenssteuer_over1mio_reform<-compute_Einkommensteuer(ZvEDurchschnitt_over1mio,gfb_reform, pg1s_reform, pg1e_reform, pg2s_reform, pg2e_reform, pp1s_reform, pp1e_reform, pp2s_reform)
+  #Durschnittliche zu zahlende Einkommensteuer für Steuerpflichtige mit zu versteuernden Einkommen über 1 Mio (Status quo und Reform)
+  Einkommensteuer_over1mio_base<-compute_Einkommensteuer(ZvEDurchschnitt_over1mio,gfb_base, pg1s_base, pg1e_base, pg2s_base, pg2e_base, pp1s_base, pp1e_base, pp2s_base)
+  Einkommensteuer_over1mio_reform<-compute_Einkommensteuer(ZvEDurchschnitt_over1mio,gfb_reform, pg1s_reform, pg1e_reform, pg2s_reform, pg2e_reform, pp1s_reform, pp1e_reform, pp2s_reform)
   #Steuereinnahmen durch Steuerpflichtige mit zu versteuernden Einkommen über 1 Mio
-  Einnahmen_over1mio_base<-AnzahlSteuerpflichte_over1mio*Einkommenssteuer_over1mio_base
-  Einnahmen_over1mio_reform<-AnzahlSteuerpflichte_over1mio*Einkommenssteuer_over1mio_reform
+  Einnahmen_over1mio_base<-AnzahlSteuerpflichte_over1mio*Einkommensteuer_over1mio_base
+  Einnahmen_over1mio_reform<-AnzahlSteuerpflichte_over1mio*Einkommensteuer_over1mio_reform
   Mindereinnahmen_over1mio<-Einnahmen_over1mio_base-Einnahmen_over1mio_reform
 
   #Creating dataset####
@@ -60,30 +60,30 @@ create_dataframeReform<-function(
     mutate(ZvE=ZvE, #Anpassung!!!
            Steuerpflichtige_norm_smooth,
            Grenzsteuersatz_base=compute_Grenzsteuersatz(ZvE,gfb_base, pg1s_base, pg1e_base, pg2s_base, pg2e_base, pp1s_base, pp1e_base, pp2s_base),
-           Einkommenssteuer_base=compute_Einkommensteuer(ZvE,gfb_base, pg1s_base, pg1e_base, pg2s_base, pg2e_base, pp1s_base, pp1e_base, pp2s_base),
+           Einkommensteuer_base=compute_Einkommensteuer(ZvE,gfb_base, pg1s_base, pg1e_base, pg2s_base, pg2e_base, pp1s_base, pp1e_base, pp2s_base),
            Grenzsteuersatz_reform=compute_Grenzsteuersatz(ZvE,gfb_reform, pg1s_reform, pg1e_reform, pg2s_reform, pg2e_reform, pp1s_reform, pp1e_reform, pp2s_reform),
-           Einkommenssteuer_reform=compute_Einkommenssteuer(ZvE,gfb_reform, pg1s_reform, pg1e_reform, pg2s_reform, pg2e_reform, pp1s_reform, pp1e_reform, pp2s_reform),
-           Durchschnittssteuersatz_base=100*Einkommenssteuer_base/ZvE,
-           Einnahmen_base=Einkommenssteuer_base*Steuerpflichtige_norm_smooth,
-           Durchschnittssteuersatz_reform=100*Einkommenssteuer_reform/ZvE,
-           Einnahmen_reform=Einkommenssteuer_reform*Steuerpflichtige_norm_smooth,
+           Einkommensteuer_reform=compute_Einkommensteuer(ZvE,gfb_reform, pg1s_reform, pg1e_reform, pg2s_reform, pg2e_reform, pp1s_reform, pp1e_reform, pp2s_reform),
+           Durchschnittssteuersatz_base=100*Einkommensteuer_base/ZvE,
+           Einnahmen_base=Einkommensteuer_base*Steuerpflichtige_norm_smooth,
+           Durchschnittssteuersatz_reform=100*Einkommensteuer_reform/ZvE,
+           Einnahmen_reform=Einkommensteuer_reform*Steuerpflichtige_norm_smooth,
            Entlastung_pcZvE=Durchschnittssteuersatz_base-Durchschnittssteuersatz_reform,
-           Entlastung_absolut=Einkommenssteuer_base-Einkommenssteuer_reform,
+           Entlastung_absolut=Einkommensteuer_base-Einkommensteuer_reform,
            Mindereinnahmen=Einnahmen_base-Einnahmen_reform
     )%>%
     add_row(Mindereinnahmen=Mindereinnahmen_over1mio)%>%
     mutate(
       #SoliGrenzsteuersatz
       Soli_Grenzsteuersatz_base=case_when(
-        is.na(Einkommenssteuer_base)~NA_real_,
-        Einkommenssteuer_base<solib_base~0,
-        (Einkommenssteuer_base-solib_base)*solig_base/100<Einkommenssteuer_base*solis_base/100~solig_base*Grenzsteuersatz_base/100,
+        is.na(Einkommensteuer_base)~NA_real_,
+        Einkommensteuer_base<solib_base~0,
+        (Einkommensteuer_base-solib_base)*solig_base/100<Einkommensteuer_base*solis_base/100~solig_base*Grenzsteuersatz_base/100,
         TRUE~solis_base*Grenzsteuersatz_base/100
       ),
       Soli_Grenzsteuersatz_reform=case_when(
-        is.na(Einkommenssteuer_reform)~NA_real_,
-        Einkommenssteuer_reform<solib_reform~0,
-        (Einkommenssteuer_reform-solib_reform)*solig_reform/100<Einkommenssteuer_reform*solis_reform/100~solig_reform*Grenzsteuersatz_reform/100,
+        is.na(Einkommensteuer_reform)~NA_real_,
+        Einkommensteuer_reform<solib_reform~0,
+        (Einkommensteuer_reform-solib_reform)*solig_reform/100<Einkommensteuer_reform*solis_reform/100~solig_reform*Grenzsteuersatz_reform/100,
         TRUE~solis_reform*Grenzsteuersatz_reform/100
       ),
       Grenzsteuersatz_inklSoli_base=Grenzsteuersatz_base+Soli_Grenzsteuersatz_base,
@@ -92,23 +92,23 @@ create_dataframeReform<-function(
       Solitotal_base=case_when(
         is.na(Soli_Grenzsteuersatz_base)~NA_real_,
         Soli_Grenzsteuersatz_base==0~0,
-        Soli_Grenzsteuersatz_base==solig_base*Grenzsteuersatz_base/100~(Einkommenssteuer_base-solib_base)*solig_base/100,
-        Soli_Grenzsteuersatz_base==solis_base*Grenzsteuersatz_base/100~Einkommenssteuer_base*solis_base/100
+        Soli_Grenzsteuersatz_base==solig_base*Grenzsteuersatz_base/100~(Einkommensteuer_base-solib_base)*solig_base/100,
+        Soli_Grenzsteuersatz_base==solis_base*Grenzsteuersatz_base/100~Einkommensteuer_base*solis_base/100
       ),
       Solitotal_reform=case_when(
         is.na(Soli_Grenzsteuersatz_reform)~NA_real_,
         Soli_Grenzsteuersatz_reform==0~0,
-        Soli_Grenzsteuersatz_reform==solig_reform*Grenzsteuersatz_reform/100~(Einkommenssteuer_reform-solib_reform)*solig_reform/100,
-        Soli_Grenzsteuersatz_reform==solis_reform*Grenzsteuersatz_reform/100~Einkommenssteuer_reform*solis_reform/100
+        Soli_Grenzsteuersatz_reform==solig_reform*Grenzsteuersatz_reform/100~(Einkommensteuer_reform-solib_reform)*solig_reform/100,
+        Soli_Grenzsteuersatz_reform==solis_reform*Grenzsteuersatz_reform/100~Einkommensteuer_reform*solis_reform/100
       ),
-      Einkommenssteuer_inklSoli_base=Einkommenssteuer_base+Solitotal_base,
-      Einkommenssteuer_inklSoli_reform=Einkommenssteuer_reform+Solitotal_reform,
-      Durchschnittssteuersatz_inklSoli_base=100*Einkommenssteuer_inklSoli_base/ZvE,
-      Einnahmen_inklSoli_base=Einkommenssteuer_inklSoli_base*Steuerpflichtige_norm_smooth,
-      Durchschnittssteuersatz_inklSoli_reform=100*Einkommenssteuer_inklSoli_reform/ZvE,
-      Einnahmen_inklSoli_reform=Einkommenssteuer_inklSoli_reform*Steuerpflichtige_norm_smooth,
+      Einkommensteuer_inklSoli_base=Einkommensteuer_base+Solitotal_base,
+      Einkommensteuer_inklSoli_reform=Einkommensteuer_reform+Solitotal_reform,
+      Durchschnittssteuersatz_inklSoli_base=100*Einkommensteuer_inklSoli_base/ZvE,
+      Einnahmen_inklSoli_base=Einkommensteuer_inklSoli_base*Steuerpflichtige_norm_smooth,
+      Durchschnittssteuersatz_inklSoli_reform=100*Einkommensteuer_inklSoli_reform/ZvE,
+      Einnahmen_inklSoli_reform=Einkommensteuer_inklSoli_reform*Steuerpflichtige_norm_smooth,
       Entlastung_inklSoli_pcZvE=Durchschnittssteuersatz_inklSoli_base-Durchschnittssteuersatz_inklSoli_reform,
-      Entlastung_inklSoli_absolut=Einkommenssteuer_inklSoli_base-Einkommenssteuer_inklSoli_reform,
+      Entlastung_inklSoli_absolut=Einkommensteuer_inklSoli_base-Einkommensteuer_inklSoli_reform,
       Mindereinnahmen_inklSoli=Einnahmen_inklSoli_base-Einnahmen_inklSoli_reform,
       AnteilZvE=cumsum(Steuerpflichtige_norm_smooth)/sum(Steuerpflichtige_norm_smooth,na.rm = T)
     )%>%
