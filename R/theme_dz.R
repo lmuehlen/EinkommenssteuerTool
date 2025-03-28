1f2197628740fc6c380236a2a1c9aaf7eaaf79b0
lmuehlen <lmuehlen@uni-muenster.de>
2025-03-28 15:44:05 +0100

    0.02

--- R/theme_dz.R
+++ R/theme_dz.R
@@ -1,6 +1,7 @@
 #' Ggplot theme Dezernat Zukunft
 #'
 #' @param base_size base size text
+#' @param base_family base font family text
 #'
 #' @import ggthemes
 #'
@@ -10,8 +11,8 @@
 #' @examples
 #' \dontrun{
 #' theme_dz()}
-theme_dz<-function(base_size=10){
-  theme_tufte(base_family = "Open Sans",base_size = base_size)+
+theme_dz<-function(base_size=10,base_family = "Open Sans"){
+  theme_tufte(base_family = base_family,base_size = base_size)+
     theme(panel.grid.major.y = element_line(linetype = "11",linewidth=0.5,color="#181c44"),
           axis.ticks.y = element_blank(),
           axis.ticks.x =element_line(color="#181c44"),
